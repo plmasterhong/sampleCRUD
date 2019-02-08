988d668cac12e0a18949a19e09aef74a66e5002b
Administrator <Administrator@smart20111>
2019-02-08 10:38:20 +0900

    [홍승윤]sampleCRUD 프로젝트 초기화

---
+++ web03/src/main/java/com/eteam/web03/ServletInitializer.java
@@ -0,0 +1,14 @@
+package com.eteam.web03;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(Web03Application.class);
+	}
+
+}
+
