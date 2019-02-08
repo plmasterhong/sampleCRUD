1b1ed8218392877b195fa3b33d09c1fd2ee5d4f2
Administrator <Administrator@smart20111>
2019-02-08 10:55:58 +0900

    [홍승윤]sample.java생성 tb-name:sample필드와 일치하는 VO

---
+++ web03/src/main/java/com/eteam/web03/vo/Sample.java
@@ -0,0 +1,22 @@
+package com.eteam.web03.vo;
+
+public class Sample {
+	private int sampleId;			// tb-name:sample, field:sample_id
+	private String sampleName;		// tb-name:sample, field:sample_name
+	public int getSampleId() {
+		return sampleId;
+	}
+	public void setSampleId(int sampleId) {
+		this.sampleId = sampleId;
+	}
+	public String getSampleName() {
+		return sampleName;
+	}
+	public void setSampleName(String sampleName) {
+		this.sampleName = sampleName;
+	}
+	@Override
+	public String toString() {
+		return "Sample [sampleId=" + sampleId + ", sampleName=" + sampleName + "]";
+	}
+}
